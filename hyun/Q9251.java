1768887639040ec5203c03d7510241661a3b4676
Hyun Hwang <hyun@Hyunui-MacBookPro.local>
2025-03-09 10:27:41 +0900

    baekjoon: gold 1

---
+++ hyun/Q9251.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Q9251 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+
+        int[][] d = new int[1001][1001];
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    d[i][j] = d[i - 1][j - 1] + 1;
+                } else {
+                    d[i][j] = Math.max(d[i - 1][j], d[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(d[s1.length()][s2.length()]);
+    }
+}
+
+// DP
+// 1000 * 1000 = 100 0000
